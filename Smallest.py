665bcaa5d88dff9eec056c3a51659d2dacaf859f
ajk338 <aaronkleyn@gmail.com>
2015-04-12 20:16:34 -0500

    Update Smallest.py

--- Smallest.py
+++ Smallest.py
@@ -1,3 +1,7 @@
+#2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+#What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
 i = 20
 
 while (i % 11 != 0 or i % 12 != 0 or i % 13 != 0 or
